08e9b2a2089262f8e21e1055d5b84df16afb5fc1
unifyh <18213435+unifyh@users.noreply.github.com>
2023-04-01 22:38:51 +0800

    Add localization

--- scripts/mods/minimap/minimap_localization.lua
+++ scripts/mods/minimap/minimap_localization.lua
@@ -1,5 +1,10 @@
 return {
+	mod_name = {
+		en = "Minimap",
+		["zh-cn"] = "小地图",
+	},
 	mod_description = {
-		en = "minimap description",
+		en = "Add a minimap to your HUD.",
+		["zh-cn"] = "在 HUD 上显示小地图。",
 	},
 }
