262a670f06f70a5549ab837a18862acb74a0201a
xsSplater <xssplater@gmail.com>
2023-04-10 10:03:17 +0300

    Update minimap_localization.lua

--- scripts/mods/minimap/minimap_localization.lua
+++ scripts/mods/minimap/minimap_localization.lua
@@ -1,14 +1,17 @@
 return {
     mod_name = {
         en = "Minimap",
-        ["zh-cn"] = "小地图",
+        ["zh-cn"] = "小地图",",
+        ru = "Миникарта",
     },
     mod_description = {
         en = "Add a minimap to your HUD.",
         ["zh-cn"] = "在 HUD 上显示小地图。",
+        ru = "Minimap - Добавляет миникарту на ваш интерфейс.",
     },
     display_class_icon = {
         en = "Display Class Icon",
         ["zh-cn"] = "显示职业图标",
+        ru = "Показывать значки классов",
     },
 }
