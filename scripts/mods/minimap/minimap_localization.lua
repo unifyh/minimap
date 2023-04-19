1cb484f5fb685948c7facd6ad9b4b0234f1b78cc
xsSplater <xssplater@gmail.com>
2023-04-19 08:43:09 +0300

    Update minimap_localization.lua (#6)

--- scripts/mods/minimap/minimap_localization.lua
+++ scripts/mods/minimap/minimap_localization.lua
@@ -17,41 +17,51 @@ return {
     icon_visibility = {
         en = "Icon Visibility",
         ["zh-cn"] = "图标可见性",
+        ru = "Видимость значков",
     },
     location_attention_vis = {
         en = "Attention Pings",
         ["zh-cn"] = "注意标记",
+        ru = "Отметки привлечения внимания",
     },
     location_ping_vis = {
         en = "Location Pings",
         ["zh-cn"] = "位置标记",
+        ru = "Отметки мест",
     },
     location_threat_vis = {
         en = "Threat Area Pings",
         ["zh-cn"] = "威胁区域标记",
+        ru = "Отметки опасных мест",
     },
     unit_threat_vis = {
         en = "Threat Units",
         ["zh-cn"] = "威胁单位",
+        ru = "Опасные враги",
     },
     player_vis = {
         en = "Players",
         ["zh-cn"] = "玩家",
+        ru = "Игроки",
     },
     assistance_vis = {
         en = "Disabled Teammates",
         ["zh-cn"] = "失能队友",
+        ru = "Выведенные из строя игроки",
     },
     objective_vis = {
         en = "Mission Objectives",
         ["zh-cn"] = "任务目标",
+        ru = "Цели миссии",
     },
     damaged_enemy_vis = {
         en = "Damaged Enemies",
         ["zh-cn"] = "受到伤害的敌人",
+        ru = "Раненые враги",
     },
     tagged_interaction_vis = {
         en = "Tagged Interactions",
         ["zh-cn"] = "被标记的可交互物品",
+        ru = "Отметки объектов взаимодействия",
     },
 }
